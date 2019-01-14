6bf3687cb89fcb735256d757d6a8c4460281bab9
dcx2202 <31130828+dcx2202@users.noreply.github.com>
2019-01-14 13:37:36 +0000

    Added Composite stuff

---
+++ YAMLEditor/IComponent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YAMLEditor
+{
+    interface IComponent
+    {
+        IComponent getChild(int i);
+        List<IComponent> getChildren();
+        void add(IComponent child);
+        void remove(IComponent child);
+        Object getValue();
+        void setValue(string value);
+    }
+}
---
+++ YAMLEditor/YamlComponent.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.RepresentationModel;
+
+namespace YAMLEditor
+{
+    class YamlComponent : IComponent
+    {
+        public List<IComponent> mChilds;
+
+        public YamlComponent(List<IComponent> aChilds = null)
+        {
+            mChilds = aChilds;
+
+        }
+
+        public void add(IComponent child)
+        {
+            mChilds.Add(child);
+        }
+
+        public void remove(IComponent child)
+        {
+            mChilds.Remove(child);
+        }
+
+        public IComponent getChild(int i)
+        {
+            if(mChilds.Count >= i)
+                return mChilds[i];
+            return null;
+        }
+
+        public List<IComponent> getChildren()
+        {
+            return mChilds;
+        }
+
+        public Object getValue()
+        {
+            return getChildren();
+        }
+
+        public void setValue(string value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
