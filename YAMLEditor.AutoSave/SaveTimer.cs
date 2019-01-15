6cc4da8fa36d1aaec12b6c05323a34eadb6b99a9
exhilaratedguy <32616250+exhilaratedguy@users.noreply.github.com>
2019-01-15 22:18:49 +0000

    cant access ICommandManager being same as Bukshap

---
+++ YAMLEditor.AutoSave/SaveTimer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using YAMLEditor.Patterns;
+
+namespace YAMLEditor.AutoSave
+{
+    class SaveTimer
+    {
+        // 1000 -> 1s
+        const int TIMER_DURATION = 30000;
+        private ICommandManager mCommandManager;
+        
+        public static void Main()
+        {
+            Timer saveTimer = new Timer(TIMER_DURATION);
+            saveTimer.Elapsed += SaveTimer_Elapsed;
+            saveTimer.Start();
+        }
+
+        private static void SaveTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Auto Saved");
+        }
+
+    }
+}
