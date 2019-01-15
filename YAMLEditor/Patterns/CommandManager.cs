6853ef1894d339157c419efe546962b9a936a3dd
diogon6 <32933699+diogon6@users.noreply.github.com>
2019-01-15 20:59:06 +0000

    Undo's and redo's done, needs testing.

---
+++ YAMLEditor/Patterns/CommandManager.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YAMLEditor.Patterns
+{
+	public class CommandManager : ICommandManager, ISubject
+	{
+		#region Fields
+
+		protected List<ICommand> Commands { get; } = new List<ICommand>();
+		protected int Position { get; set; } = -1;
+
+		public event UpdateEventHandler OnUpdate;
+
+		#endregion
+
+		/// <summary>
+		/// Determines if exists commands to be undoed.
+		/// </summary>
+		public bool HasUndo()
+		{
+			return (Position > -1);
+		}
+
+		/// <summary>
+		/// Determines if exists commands to be redoed.
+		/// </summary>
+		public bool HasRedo()
+		{
+			return (Position < Commands.Count - 1);
+		}
+
+		/// <summary>
+		/// Undo the last executed or redoed command
+		/// </summary>
+		public void Undo()
+		{
+			if (!HasUndo()) return;
+			Commands[Position].Undo();
+			Position = Position - 1;
+			Notify();
+		}
+
+		/// <summary>
+		/// Redo the previous undoed command.
+		/// </summary>
+		public void Redo()
+		{
+			if (!HasRedo()) return;
+			Position = Position + 1;
+			Commands[Position].Redo();
+			Notify();
+		}
+
+		/// <summary>
+		/// Executes the command.
+		/// </summary>
+		public void Execute(ICommand aCommand)
+		{
+			if (HasRedo())
+			{
+				Commands.RemoveRange(Position + 1, Commands.Count - Position - 1);
+			}
+			aCommand.Execute();
+			Commands.Add(aCommand);
+			Position = Commands.Count - 1;
+
+			Notify();
+		}
+
+		public void Notify(object aData = null)
+		{
+			OnUpdate?.Invoke(this, aData);
+		}
+	}
+}
---
+++ YAMLEditor/Patterns/ISubject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YAMLEditor.Patterns
+{
+	public delegate void UpdateEventHandler(ISubject aSubject, object aData);
+	public interface ISubject
+	{
+		event UpdateEventHandler OnUpdate;
+		void Notify(object aData);
+	}
+}
