e9fc9d7eb6df4c146160e024c31313cdf16210dd
diogon6 <32933699+diogon6@users.noreply.github.com>
2019-01-17 14:08:19 +0000

    Wizard progress

--- YAMLEditor/NewComponent.Designer.cs
+++ YAMLEditor/NewComponent.Designer.cs
@@ -30,28 +30,21 @@
 		{
 			this.components = new System.ComponentModel.Container();
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NewComponent));
-			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
 			this.pictureBox1 = new System.Windows.Forms.PictureBox();
 			this.label1 = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
 			this.button1 = new System.Windows.Forms.Button();
 			this.button2 = new System.Windows.Forms.Button();
-			this.menuStrip2 = new System.Windows.Forms.MenuStrip();
 			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
 			this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
 			this.contextMenuStrip3 = new System.Windows.Forms.ContextMenuStrip(this.components);
-			this.menuStrip4 = new System.Windows.Forms.MenuStrip();
+			this.label3 = new System.Windows.Forms.Label();
+			this.File = new System.Windows.Forms.TextBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.label4 = new System.Windows.Forms.Label();
+			this.linkLabel1 = new System.Windows.Forms.LinkLabel();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.SuspendLayout();
-			// 
-			// menuStrip1
-			// 
-			this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-			this.menuStrip1.Location = new System.Drawing.Point(0, 24);
-			this.menuStrip1.Name = "menuStrip1";
-			this.menuStrip1.Size = new System.Drawing.Size(822, 24);
-			this.menuStrip1.TabIndex = 0;
-			this.menuStrip1.Text = "menuStrip1";
 			// 
 			// pictureBox1
 			// 
@@ -90,7 +83,7 @@
 			// 
 			this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
 			this.button1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button1.Location = new System.Drawing.Point(397, 287);
+			this.button1.Location = new System.Drawing.Point(449, 311);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(84, 43);
 			this.button1.TabIndex = 6;
@@ -101,21 +94,13 @@
 			// 
 			this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
 			this.button2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button2.Location = new System.Drawing.Point(600, 287);
+			this.button2.Location = new System.Drawing.Point(647, 311);
 			this.button2.Name = "button2";
 			this.button2.Size = new System.Drawing.Size(91, 43);
 			this.button2.TabIndex = 7;
 			this.button2.Text = "Cancel";
 			this.button2.UseVisualStyleBackColor = false;
-			// 
-			// menuStrip2
-			// 
-			this.menuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
-			this.menuStrip2.Location = new System.Drawing.Point(0, 0);
-			this.menuStrip2.Name = "menuStrip2";
-			this.menuStrip2.Size = new System.Drawing.Size(822, 24);
-			this.menuStrip2.TabIndex = 8;
-			this.menuStrip2.Text = "menuStrip2";
+			this.button2.Click += new System.EventHandler(this.CloseButton);
 			// 
 			// contextMenuStrip1
 			// 
@@ -135,31 +120,72 @@
 			this.contextMenuStrip3.Name = "contextMenuStrip3";
 			this.contextMenuStrip3.Size = new System.Drawing.Size(61, 4);
 			// 
-			// menuStrip4
+			// label3
 			// 
-			this.menuStrip4.Dock = System.Windows.Forms.DockStyle.None;
-			this.menuStrip4.ImageScalingSize = new System.Drawing.Size(20, 20);
-			this.menuStrip4.Location = new System.Drawing.Point(450, 191);
-			this.menuStrip4.Name = "menuStrip4";
-			this.menuStrip4.Size = new System.Drawing.Size(158, 28);
-			this.menuStrip4.TabIndex = 13;
-			this.menuStrip4.Text = "none";
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(325, 208);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(43, 21);
+			this.label3.TabIndex = 10;
+			this.label3.Text = "File: ";
+			// 
+			// File
+			// 
+			this.File.Location = new System.Drawing.Point(369, 208);
+			this.File.Name = "File";
+			this.File.Size = new System.Drawing.Size(384, 22);
+			this.File.TabIndex = 11;
+			// 
+			// button3
+			// 
+			this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+			this.button3.Location = new System.Drawing.Point(759, 199);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(51, 42);
+			this.button3.TabIndex = 12;
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.FileExplorer);
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(90, 311);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(203, 21);
+			this.label4.TabIndex = 13;
+			this.label4.Text = "For more components visit:";
+			// 
+			// linkLabel1
+			// 
+			this.linkLabel1.AutoSize = true;
+			this.linkLabel1.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.linkLabel1.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.linkLabel1.Location = new System.Drawing.Point(27, 349);
+			this.linkLabel1.Name = "linkLabel1";
+			this.linkLabel1.Size = new System.Drawing.Size(329, 21);
+			this.linkLabel1.TabIndex = 14;
+			this.linkLabel1.TabStop = true;
+			this.linkLabel1.Text = "https://www.home-assistant.io/components/";
+			this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkButton);
 			// 
 			// NewComponent
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(822, 399);
+			this.Controls.Add(this.linkLabel1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.File);
+			this.Controls.Add(this.label3);
 			this.Controls.Add(this.button2);
 			this.Controls.Add(this.button1);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.pictureBox1);
-			this.Controls.Add(this.menuStrip1);
-			this.Controls.Add(this.menuStrip2);
-			this.Controls.Add(this.menuStrip4);
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.MainMenuStrip = this.menuStrip1;
 			this.Name = "NewComponent";
 			this.Text = "NewComponent";
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -169,17 +195,18 @@
 		}
 
 		#endregion
-
-		private System.Windows.Forms.MenuStrip menuStrip1;
 		private System.Windows.Forms.PictureBox pictureBox1;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.Button button2;
-		private System.Windows.Forms.MenuStrip menuStrip2;
 		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
 		private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
 		private System.Windows.Forms.ContextMenuStrip contextMenuStrip3;
-		private System.Windows.Forms.MenuStrip menuStrip4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox File;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.LinkLabel linkLabel1;
 	}
 }
--- YAMLEditor/NewComponent.cs
+++ YAMLEditor/NewComponent.cs
@@ -12,6 +12,9 @@ namespace YAMLEditor
 {
 	public partial class NewComponent : Form
 	{
+		public string filename;
+		public string openedfilename;
+
 		public NewComponent()
 		{
 			InitializeComponent();
@@ -20,6 +23,28 @@ namespace YAMLEditor
 		private void pictureBox1_Click(object sender, EventArgs e)
 		{
 
+		}
+
+		private void FileExplorer(object sender, EventArgs e)
+		{
+			var dialog = new OpenFileDialog()
+			{ Filter = @"Yaml files (*.yaml)|*.yaml|All files (*.*)|*.*", DefaultExt = "yaml" };
+			if (dialog.ShowDialog() == DialogResult.OK)
+			{
+				openedfilename = dialog.FileName;
+				filename = openedfilename;
+				File.Text = filename;
+			}
+		}
+
+		private void CloseButton(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void LinkButton(object sender, LinkLabelLinkClickedEventArgs e)
+		{
+			System.Diagnostics.Process.Start("https://www.home-assistant.io/components/");
 		}
 	}
 }
